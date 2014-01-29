1cd5f3aa9abf7a17cb635faaa9504265c3dcaaad
Ahmad Amireh <ahmad.amireh@gmail.com>
2014-01-29 13:28:22 +0200

    [broken] observer should re-observe after #unobserve()

--- spec/rivets/observer.js
+++ spec/rivets/observer.js
@@ -22,4 +22,60 @@ describe("Rivets.observer", function() {
     model.a.b.c = 'baz';
     expect(called_count).toBe(1);
   });
+
+  describe('suite', function() {
+    var originalAdapter, nrSubscriptions;
+
+    beforeEach(function() {
+      nrSubscriptions = 0;
+      originalAdapter = rivets.adapters[':'];
+      var newAdapter = {
+        subscribe: function(obj, keypath, callback) {
+          console.debug('Subscribed to:', keypath);
+          ++nrSubscriptions;
+        },
+
+        unsubscribe: function(obj, keypath, callback) {
+          console.debug('Unsubscribed from:', keypath);
+          --nrSubscriptions;
+        },
+
+        read: function(obj, keypath) {
+          return obj[keypath];
+        },
+
+        publish: function(obj, keypath, value) {
+          obj[keypath] = value;
+        }
+      };
+
+      rivets.adapters[':'] = newAdapter;
+    });
+
+    afterEach(function() {
+      rivets.adapters[':'] = originalAdapter;
+    });
+
+    it('should unsubscribe for each subscribed keypath', function() {
+      var el = document.createElement('div');
+      el.innerHTML = '<ul><li rv-each-item="items">{ :item }</li></ul>';
+      var view = rivets.bind(el, {});
+
+      view.update({ items: [ 'A', 'B', 'C' ] });
+      expect(nrSubscriptions).toEqual(3);
+
+      view.unbind();
+      expect(nrSubscriptions).toEqual(0); // fails
+
+      view.bind();
+      expect(nrSubscriptions).toEqual(3);
+
+      // view.update({ items: [ 'A', 'B', 'C', 'D' ] });
+      // expect(nrSubscriptions).toEqual(4);
+
+      // view.unbind();
+      // expect(nrSubscriptions).toEqual(0);
+    });
+  });
+
 });
